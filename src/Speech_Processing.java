bd2b483871ef61e91304219b8561bd7079d66606
glasscockj <glasscockj@WITPF0TYDXM.wit.private>
2021-05-21 14:39:00 -0400

    Debiug of AudioInputStream object inclusion

--- src/Speech_Processing.java
+++ src/Speech_Processing.java
@@ -8,24 +8,47 @@
 import java.lang.*;
 import java.util.*;
 import javax.sound.*;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.AudioInputStream;
 import java.io.*;
 
 
 /*
  * Will include a main in this file 
  */
-public class Speech_Processing {
+public class Speech_Processing{
 	BandPassFilter filter;
-	InputStream audio = new InputStream();
-	d_vectors = new LinkedList(); //could create a linked list of d vector objects
+	File audioFile;
+	AudioInputStream audioStream;
+	LinkedList d_vectors = new LinkedList(); //could create a linked list of d vector objects
 	private double windowDelta;
 	private int windowAmount;
-	private int dVectorsTotal = 0;
-	private int trainedSpeakers = 0;
-	private static int embeddingLength = 128;
-	private static double windowSize = 1; // in seconds
-	private static double windowOverlap = 0.5; //in seconds
+	private int dVectorsTotal;
+	private int trainedSpeakers;
+	private static int embeddingLength;
+	private static double windowSize; // in seconds
+	private static double windowOverlap; //in seconds
 	String speakerNames[] = {null}; //might check to see if an array is a good tool for this or not
+	
+	//constructor
+	Speech_Processing(File input){
+		this.audioFile = input;
+		dVectorsTotal = 0;
+		trainedSpeakers = 0;
+		embeddingLength = 128;
+		windowSize = 1;
+		windowOverlap = 0.5;
+		
+		try {
+			AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+		} catch (IOException e) {
+			
+		}
+		catch(UnsupportedAudioFileException f) {
+			
+		}
+	}
 	
 	//Now for the methods
 	protected double vectorSimilarity(double vector1[], double vector2[]) {
@@ -65,19 +88,18 @@ public class Speech_Processing {
 		//make sure audio is sampled at the correct frequency, and if not,
 		//resample it
 		if(fs < 16000) {
-			//audio = resampleAudio(audioObject, audio, fs, 16000);
+			//to resample, just reset the sample rate of the audio possibly by using a factor
+			//possibly create new audio stream or audio handling file
+			//audioStream = resampleAudio(audioStream, audio, fs, 16000);
 			fs = 16000;
 		}
 		
-		//If the audio is not a column vector, reshape it
-		//TODO: figure out the 
-		if(!(audio.length)) {
+		//normalize audio
+		//data = obj.bandpass_filter.filter(audio);
 			
-		}
-		
 	}
-	private void detectSpeech() {
-		
+	private void resampleAudio(AudioInputStream originalFormat, int currentFS, int newFS) {
+		//TODO: Implement this method, based on resample information from method call in preprocessing method
 	}
 	
 	public int[] makeAudioWindows(int fs, double windowSize, double windowOverlap) {//check to make sure an int array is appropriate
@@ -93,7 +115,7 @@ public class Speech_Processing {
 		//pre-allocate the windows matrix
 		windowDelta = windowSize - windowOverlap;
 		//issue of length(audio) from the Matlab. Once I know the audio file type I can research documentation
-		windowAmount = floor(length(audio) / windowDelta);)//floor in Java goes towards positive inf and Matlab goes towards negative inf
+		windowAmount = (int) audioStream.getFrameLength() / (int) windowDelta;//floor in Java goes towards positive inf and Matlab goes towards negative inf
 		int[] windows = new int[windowAmount];
 
 		//check to make sure the clip is long enough to break into
@@ -107,38 +129,46 @@ public class Speech_Processing {
 			windows[i] = 0;
 		}
 		//Continue the rest of this later
-		if(length(audio) < window_size) {
+		if(audioStream.getFrameLength() < windowSize) {
 			//TODO: debug the audio length
 		}
+		
+		
+		
+		
+		
+		return windows;
 	}
-	public <T> void getEmbeddings(<T> obj, <T> audio, int fs) {//update the parameters
+	//public <T> void getEmbeddings(<T> obj, <T> audio, int fs) {//update the parameters
 		//TODO: implement and debug
 		//Convert window size and overlap to sample domain
-		int[] windows = makeAudioWindows(obj, audio, fs);
+		//int[] windows = makeAudioWindows(obj, audio, fs);
 	
 		//if the window is empty, return nothing
-		if(windows.isEmpty()) {
-			return null;
-		}
+		//if(windows.isEmpty()) {
+			//return null;
+		//}
 		
 		//grab embeddings for each window
 		//set the embeddings to zero to start with
-		int embeddings[windows.length][128]; 
-		for(int emb = 1; emb < windows.length; i++) {
-			for(int i = 0; i < 128; i++) {
-				embeddings[emb][j] = vggishFeatures();//update parameters
-			}
-		}
-	}
+		//int embeddings[windows.length][128]; 
+		//for(int emb = 1; emb < windows.length; i++) {
+			//for(int i = 0; i < 128; i++) {
+				//embeddings[emb][j] = vggishFeatures();//update parameters
+			//}
+		//}
+	//}
 	
 	//returns the processed speech array for the data attributed to each speaker
 	int getSpeechSpeakerIndices( int fs, int windows[],Speaker speakers) {
 		//TODO: implement and debug
+		int index = 0;
 		//Convert window size and over
 		double window_size_samples = this.windowSize * fs;
 		double window_overlap_samples = this.windowOverlap * fs;
 		double window_delta_samples = window_size_samples - window_overlap_samples;
 		
+		return index;
 		
 	}
 	
@@ -147,9 +177,11 @@ public class Speech_Processing {
 	 * Main method to do the work of the audio processing
 	 */
 	public static void main() {
+		File audioFile = new File("D:\\John\\Memorex USB\\College\\Academics\\Fourth Year\\Senior Design\\Coding\\Java Implementation\\Diarization\\GodOnlyKnows.wav");
 		
-		
-		
+		//Testing the creation of the AudioInput Stream
+		Speech_Processing test =  new Speech_Processing(audioFile);
+		System.out.println("");
 	}
 }
 //d vector is a struct, [128, n]
